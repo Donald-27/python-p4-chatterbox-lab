01b690df7474ff28f99a0c09f925fc61ddb5df31
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-19 11:20:21 +0300

    Build full CRUD Flask API for Chatterbox messages

--- server/app.py
+++ server/app.py
@@ -11,16 +11,49 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/')
+def index():
+    return '<h1>Chatterbox API</h1>'
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# GET all messages
+@app.route('/messages', methods=['GET'])
+def messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in messages]), 200
+
+# POST a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    try:
+        new_msg = Message(username=data['username'], body=data['body'])
+        db.session.add(new_msg)
+        db.session.commit()
+        return jsonify(new_msg.to_dict()), 201
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+# PATCH a message
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+
+    if 'body' in data:
+        message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict()), 200
+    return {"error": "No body provided"}, 400
+
+# DELETE a message
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return {"message": "Message deleted"}, 200
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
